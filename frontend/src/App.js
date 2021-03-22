70cc8ac0172eae35aa6b6da7a520736ac32cc2e5
Tim M <timothy.romero.manas@gmail.com>
2021-03-21 22:26:49 -0400

    Adding React components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreeen from './screens/HomeScreen'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Header />
+      <main>
+        <Container>
+          <Route path='/' component={HomeScreeen} exact />
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
