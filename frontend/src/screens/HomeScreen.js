3f4a8dc56d9675f51baa69f18b8db6ddd23312e4
Tim M <timothy.romero.manas@gmail.com>
2021-03-22 22:12:16 -0400

    Added FE + BE components to homescreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
+import { listProducts } from '../actions/productActions'
+import { useDispatch, useSelector } from 'react-redux'
 
 const HomeScreeen = () => {
-  return <h1>HomeScreen</h1>
+  // Get list of products
+
+  const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList)
+  const { products } = productList
+
+  useEffect(() => {
+    dispatch(listProducts())
+  }, [dispatch])
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  )
 }
 
 export default HomeScreeen
