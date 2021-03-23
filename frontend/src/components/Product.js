e950b01a54766b6a531a9d3bd8551996229d5811
Tim M <timothy.romero.manas@gmail.com>
2021-03-22 22:16:22 -0400

    Fixed bug

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,7 @@ const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded text-white bg-primary'>
       <Link to={`/product/${product._id}`}>
-        <Card.Img variant='top' src='holder.js/100px180' />
+        <Card.Img src={product.image} variant='top' />
       </Link>
 
       <Card.Body>
