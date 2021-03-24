57753f767ef8e57481704bcc88807fcd2f3b6c08
Tim M <timothy.romero.manas@gmail.com>
2021-03-24 18:51:15 -0400

    Fetch user profile after authentication

--- backend/userController.js
+++ backend/userController.js
@@ -18,3 +18,21 @@ export const authUser = asyncHandler(async (req, res) => {
     res.json({ error: 'User not found' })
   }
 })
+
+// @desc    Get User Profile
+// @route   GET /api/users/profile
+// @access  Private
+export const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    res.json({ error: 'User not found' })
+  }
+})
--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { authUser } from './userController.js'
+import { authUser, getUserProfile } from './userController.js'
+import { protect } from './authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/login').post(authUser)
+router.route('/profile').get(protect, getUserProfile)
 
 export default router
