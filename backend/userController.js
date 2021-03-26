a7b54fc21578fea1dee2750057ef29c3518a636a
Tim M <timothy.romero.manas@gmail.com>
2021-03-25 20:03:04 -0400

    Set up the routes on the backend

--- backend/userController.js
+++ backend/userController.js
@@ -36,3 +36,27 @@ export const getUserProfile = asyncHandler(async (req, res) => {
     res.json({ error: 'User not found' })
   }
 })
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+  if (userExists) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  })
+
+  if (user) {
+    res.status(200)
+    res.json({ name, email, password })
+  } else {
+    res.status(400)
+    throw new Error('Invalid User')
+  }
+})
--- backend/userRoutes.js
+++ backend/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { authUser, getUserProfile } from './userController.js'
+import { authUser, getUserProfile, registerUser } from './userController.js'
 import { protect } from './authMiddleware.js'
 
 const router = express.Router()
 
 router.route('/login').post(authUser)
 router.route('/profile').get(protect, getUserProfile)
+router.route('/').post(registerUser)
 
 export default router
